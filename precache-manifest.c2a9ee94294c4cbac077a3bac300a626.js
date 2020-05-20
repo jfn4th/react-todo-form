ea1507356820c1e9a620e05745fddad56ad609f3
Jamie Nelson <jfn4th@gmail.com>
2020-05-19 23:27:45 -0500

    Updates

---
+++ precache-manifest.c2a9ee94294c4cbac077a3bac300a626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e460bf557cbd5d0ae166a686713de5bf",
+    "url": "/react-todo-form/index.html"
+  },
+  {
+    "revision": "c3ff5382c61a218c1631",
+    "url": "/react-todo-form/static/css/main.33b0d5f0.chunk.css"
+  },
+  {
+    "revision": "47521916fb70677fed0f",
+    "url": "/react-todo-form/static/js/2.c3df7d47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-form/static/js/2.c3df7d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ff5382c61a218c1631",
+    "url": "/react-todo-form/static/js/main.26d24c60.chunk.js"
+  },
+  {
+    "revision": "cb49b5bdac43eae79485",
+    "url": "/react-todo-form/static/js/runtime-main.fce7f008.js"
+  }
+]);
